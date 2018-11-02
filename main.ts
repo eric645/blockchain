08a7af4696d612b6cba87954f4cec6d5b1257d52
eric645 <37544618+eric645@users.noreply.github.com>
2018-11-02 19:03:17 -0400

    e

--- main.ts
+++ main.ts
@@ -1,7 +1,50 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+enum block {
+    //% block="1"
+    one,
+    //% block="spend"
+    neg
+}
+block[] blocksvalues = [];
+//% width="100" color=#00FFFF icon="C"
+namespace blockchain {
+    //% block="%x"
+    //% js="%x"
+    function _Block(x) { return x; }
+    //% block="add block %x to blockchain"
+    function addblock(x: block) {
+        blocksvalues.push(_repr(x));
+    }
+    //% block="repr %x"
+    function _repr(x: block) {
+        if (x === block.one) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+    //% block="values"
+    function values() { return blocksvalues; }
+    //% block="total"
+    function total() {
+        let p = 0;
+        let f = 0;
+        for (e in blocksvalues) {
+            f = _repr(e);
+            if (f < 0 && p <= 0) {
+                f = 0;
+            }
+            p += f;
+        }
+        return p;
+    }
+    //% block="send total"
+    function sendTotal() {
+        serial.redirect(SerialPin.P0, SerialPin.P2, 111640));
+        serial.writeString(total().toString());
+    }
+    //% block="receive a total"
+    function receiveTotal() {
+        serial.redirect(SerialPin.P0, SerialPin.P2, 111640));
+        return Number(serial.readString());
+    }
+}
